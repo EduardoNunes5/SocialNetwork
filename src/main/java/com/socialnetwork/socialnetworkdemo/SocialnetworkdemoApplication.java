16fd736a78e292d877cfa2d14feaa2bde8fe4f77
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-09-26 16:59:36 -0300

    projeto começado

---
+++ src/main/java/com/socialnetwork/socialnetworkdemo/SocialnetworkdemoApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.socialnetworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkdemoApplication.class, args);
+	}
+
+}
