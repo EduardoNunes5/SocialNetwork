c882c4b7cdf86db168cbf59d9bff75f05d3e9468
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-01 16:04:29 -0300

    user registration hashes password

--- src/main/java/com/socialnetwork/socialnetworkdemo/services/UserService.java
+++ src/main/java/com/socialnetwork/socialnetworkdemo/services/UserService.java
@@ -3,21 +3,32 @@ package com.socialnetwork.socialnetworkdemo.services;
 import com.socialnetwork.socialnetworkdemo.entities.User;
 import com.socialnetwork.socialnetworkdemo.repositories.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService{
 
-    @Autowired
     private UserRepository userDAO;
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userDAO, PasswordEncoder passwordEncoder){
+        this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     public User createUser(User user){
         if(this.userDAO.findUserByEmail(user.getEmail()).isPresent()){
             throw new IllegalArgumentException("usuario ja existe");
         }
-        return userDAO.save(user);
+        User newUser = new User();
+        newUser.setAge(user.getAge());
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        return userDAO.save(newUser);
     }
 
 
